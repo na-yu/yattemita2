12f5a1772f697c13bbae34a299c0e9e3218ae904
na-yu <yukinakai1027@gmail.com>
2021-09-11 05:29:05 +0900

    Initial commit

--- pscweb2/settings.py
+++ pscweb2/settings.py
@@ -95,8 +95,8 @@ WSGI_APPLICATION = 'pscweb2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = { 'default': dj_database_url.config(conn_max_age=500) }
 
 
 # Password validation
